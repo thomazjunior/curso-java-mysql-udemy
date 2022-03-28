e5bddb618eae9f2471e8a94226c9f2b57b78316b
thomazjunior <p.thomaz.junior@gmail.com>
2022-03-28 17:28:12 +0100

    Secao de leitura de dados

--- exercicio_section4_estruturasequencial/ExercicioEntradaDeDados.java
+++ exercicio_section4_estruturasequencial/ExercicioEntradaDeDados.java
@@ -1,10 +1,16 @@
 package exercicio_section4_estruturasequencial;
 
+import java.util.Scanner;
+
 public class ExercicioEntradaDeDados {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Scanner = classe para leitura de dados 
+		Scanner sc = new Scanner(System.in);
+		/// NextLine = le a entrada de dados ate a primeira quebra de linha
+		String s1 = sc.nextLine();
+		System.out.print("Dados digitados: ");
+		System.out.println(s1);
 	}
 
 }
