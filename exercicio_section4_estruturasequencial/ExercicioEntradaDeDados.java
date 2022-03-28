11f0bd9a2c83ee1d0d9d42c623e131627ac09f54
thomazjunior <p.thomaz.junior@gmail.com>
2022-03-28 16:35:41 +0100

    Exercicio entrada de dados

---
+++ exercicio_section4_estruturasequencial/ExercicioEntradaDeDados.java
@@ -0,0 +1,10 @@
+package exercicio_section4_estruturasequencial;
+
+public class ExercicioEntradaDeDados {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- exercicio_section4_estruturasequencial/ExercicioSaidaDeDados.java
+++ exercicio_section4_estruturasequencial/ExercicioSaidaDeDados.java
@@ -1,27 +1,26 @@
-package exercicio_section4_saidadedados;
+package exercicio_section4_estruturasequencial;
 
-public class Main {
+public class ExercicioSaidaDeDados {
 
 	public static void main(String[] args) {
 		String product1 = "Computer";
 		String product2 = "Office desk";
-		
+
 		int age = 30;
 		int code = 5290;
 		char gender = 'F';
-		
+
 		double price1 = 2100.0;
 		double price2 = 650.50;
 		double measure = 53.234567;
-		
+
 		System.out.print("Products: ");
 		System.out.printf("%n%s, which price is $ %.2f", product1, price1);
 		System.out.printf("%n%s, which price is $ %.2f", product2, price2);
-		
-		System.out.printf("%nMeasure with eight decimal places: %.8f", measure );
-		
-		System.out.printf("%nRouded (three decimal places): %.3f", measure);
-	}
-	
 
+		System.out.printf("%nMeasure with eight decimal places: %.8f", measure);
+
+		System.out.printf("%nRouded (three decimal places): %.3f%n", measure);
+
+	}
 }
