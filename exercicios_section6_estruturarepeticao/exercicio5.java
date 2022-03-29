436b0f89e63569132a7cf8149b682a99bfef73d7
thomazjunior <p.thomaz.junior@gmail.com>
2022-03-29 16:59:49 +0100

    Exercicio da sessao 8 realizado sem usar OO

---
+++ exercicios_section6_estruturarepeticao/exercicio5.java
@@ -0,0 +1,21 @@
+package exercicios_section6_estruturarepeticao;
+
+import java.util.Scanner;
+
+public class exercicio5 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		
+		int fat = 1;
+		for (int i=1; i<=n; i++) {
+			fat = fat * i;
+		}
+		
+		System.out.println(fat);
+		
+		sc.close();
+	}
+}
