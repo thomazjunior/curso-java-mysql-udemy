fc55fada415842a20bed54a3af165c41875b29f0
thomazjunior <p.thomaz.junior@gmail.com>
2022-03-29 17:33:39 +0100

    Exercicio da unidade 8 resolvido com principios da OO

--- exercicios_section8_oo/Program.java
+++ exercicios_section8_oo/Program.java
@@ -7,22 +7,22 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
-		double xA, xB, xC, yA, yB, yC;
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
 		
 		System.out.println("Enter the measures of triangle X: ");
-		xA = sc.nextDouble();
-		xB = sc.nextDouble();
-		xC = sc.nextDouble();
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
 		
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = sc.nextDouble();
-		yB = sc.nextDouble();
-		yC = sc.nextDouble();
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
 		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * p * (p - xB) * p * (p - xC));
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * p * (p - xB) * p * (p - xC));
+		double areaX = x.area();
+		double areaY = y.area();
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
